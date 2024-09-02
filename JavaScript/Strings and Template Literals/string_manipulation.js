b597f5ce913e20d3d4b6b921428c2fee222782b7
Harini-54 <99632688+Harini-54@users.noreply.github.com>
2024-09-02 13:23:50 +0530

    Add files via upload

---
+++ JavaScript/Strings and Template Literals/string_manipulation.js
@@ -0,0 +1,26 @@
+let str1="Hello";
+let str2 = "Harini!";
+console.log("string concatenation(str1+str2): "+str1+" "+str2);
+console.log("Indexing:");
+console.log("Indexing(str1[4]): "+str1[4]);
+console.log("Indexing(str1[0]): "+str1[0]);
+console.log("Indexing(str1[7]): "+str1[7]);
+console.log("Indexing(str1[-1]): "+str1[-1]);
+console.log("Length of str1: "+ str1.length);
+console.log("Sub strings:");
+console.log("Substring of str1(1,4): "+str1.substring(1,4));
+console.log("Substring of str1(4): "+str1.substring(4));
+
+/*
+--output--
+string_manipulation.js:3 string concatenation(str1+str2): Hello Harini!
+string_manipulation.js:4 Indexing:
+string_manipulation.js:5 Indexing(str1[4]): o
+string_manipulation.js:6 Indexing(str1[0]): H
+string_manipulation.js:7 Indexing(str1[7]): undefined
+string_manipulation.js:8 Indexing(str1[-1]): undefined
+string_manipulation.js:9 Length of str1: 5
+string_manipulation.js:10 Sub strings:
+string_manipulation.js:11 Substring of str1(1,4): ell
+string_manipulation.js:12 Substring of str1(4): o
+*/
---
+++ JavaScript/Strings and Template Literals/template_literals.js
@@ -0,0 +1,20 @@
+let name = "Harini";
+let book = "Harry Potter and the Philosopher's Stone";
+let author = "J.K. Rowling";
+let description = `
+The title of the book is ${book}.
+The author of the book is ${author}.
+This book follows Harry Potter, a young wizard who discovers his magical heritage on his eleventh birthday when he receives a letter of acceptance to Hogwarts School of Witchcraft and Wizardry.
+`;
+let greet = `Welcome ${name}!`;
+console.log(greet);
+console.log("Description of the book: "+description);
+
+/*
+--output--
+Welcome Harini!
+Description of the book: 
+The title of the book is Harry Potter and the Philosopher's Stone.
+The author of the book is J.K. Rowling.
+This book follows Harry Potter, a young wizard who discovers his magical heritage on his eleventh birthday when he receives a letter of acceptance to Hogwarts School of Witchcraft and Wizardry.
+*/
